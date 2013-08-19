22eddc4aab67ceab442647c22d32417725f60d79
Bryan Myers <bryan@uhl.com>
2013-08-19 14:05:26 -0700

    Rake db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130819205451) do
+
+  create_table "installations", force: true do |t|
+    t.string   "address"
+    t.string   "city"
+    t.integer  "zip"
+    t.integer  "users_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "installations", ["users_id"], name: "index_installations_on_users_id"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "best_time"
+    t.integer  "e_bill"
+    t.integer  "access"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
