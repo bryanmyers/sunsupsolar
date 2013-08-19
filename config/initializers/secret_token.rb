7c2fc433a026502343660cfcb29d01c75438413d
Bryan Myers <bryan@uhl.com>
2013-08-19 11:21:08 -0700

    New Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SunsUpSolar::Application.config.secret_key_base = '5098dcc35fb8cd1e4e6bd4321c991e540d4f9fbf6a3f7e6a2a16d75a1767108038b1b6e1ae3d1c418b6e2ab6a3b36117c76771d7cecd7a71220913b25a4c0e81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SunsUpSolar::Application.config.session_store :cookie_store, key: '_SunsUpSolar_session'
