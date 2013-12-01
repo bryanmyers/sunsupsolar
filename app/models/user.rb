bda651e4a32faecf3bac7f203b7e72d18a2db41c
Bryan Myers <bryanmyers831@gmail.com>
2013-11-30 16:12:32 -0800

    User Model - many
    did the rails cast changes, refactored the validations, and added secure
    password and password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,28 @@
 class User < ActiveRecord::Base
 
+  # because we are using bcrypt
   has_secure_password
 
   has_many :news, foreign_key: "user_id"
 
+  # keeps silly mistakes from happening during login
   before_save { email.downcase! }
 
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :address,  presence: true
+  # Validates Presence. Length validation also implies presence
+  validates :address, :best_time, :e_bill, :password, presence: true
+  
+  # These validate type and/or length
+  validates :e_bill, numericality: true
+  validates :name, length: { maximum: 50 }
+  # vvalidates :phone, numericality: true, length: { is: 10}
+  validates :zip, numericality: true, length: { is: 5}
+  
+  # Are what they are imputting a valid email and is it already in the DB?
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  # validates :password_digest, presence: true, length: { minimum: 6 }
-  # validates :password_confirmation, presence: true
-  validates :phone, numericality: true, length: { is: 10}
-  validates :zip, numericality: true, length: { is: 5}
-  validates :best_time, presence: true
-  validates :e_bill, numericality: true
 
-
-  private
+  # Validate that the passwords match. Also implies password confirmation presence.          
+  validates :email, confirmation: true
 
 end
