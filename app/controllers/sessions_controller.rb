db7e6e5322a889c407c7d61d1fc5269562b93e62
Bryan Myers <bryanmyers831@gmail.com>
2013-11-30 16:41:36 -0800

    Sessions Controller - Railscast

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,27 @@ def new
 end
 
 def create
-  user = User.find_by_email(params[:signin][:email].downcase)
-                                .try(:authenticate, params[:signin][:password])
-  if user && user.authenticate(params[:signin][:password])
-    sign_in user
-    redirect_to user_path
+  user = User.find_by_email(params[:email])
+
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Logged in!"
+
   else
     flash.now[:error] = 'Invalid email/password combination'
     render 'new'
   end
 end
 
-def destroy
-  sign_out
-  redirect_to root_path
-end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
+  end
 
+private
+
+def session_params
+  params_require(:email, :password).permit(:email, :password)
+end
 
 end
