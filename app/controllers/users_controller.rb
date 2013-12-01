4f1448627e4eee4453ec70522df417c123a4d4c8
Bryan Myers <bryanmyers831@gmail.com>
2013-11-30 16:14:22 -0800

    Users Controller - Railscast
    Added methods needed for full user resource, added phone field
    permission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,18 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
   # GET /users/new
   def new
     @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
   end
 
   # POST /users
@@ -23,6 +32,20 @@ class UsersController < ApplicationController
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -46,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :phone, :address, :zip, :best_time, :e_bill, :access, :password)
+      params.require(:user).permit(:address, :best_time, :e_bill, :email, :name, :password, :password_confirmation, :phone, :zip)
     end
 end
